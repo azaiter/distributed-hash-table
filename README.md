c010c809af9b028285783a59ef5da3cc68b633ca
Zaiter <azaiter@svsu.edu>
2019-04-07 16:56:01 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,105 @@
+# Simple Distributed Hash Table
+
+Author: Abdulrahman (Zack) Zaiter
+
+This program is a proof of concept of simple cloud-native distributed hash table implementation, each peer is a docker container within a contained docker-compose network.
+
+## Requirements:
+- Docker and Docker-Compose
+- That's it.
+
+## To Run:
+- `docker-compose down; docker-compose build; docker-compose up`
+- That's it
+
+# Current design vs. Other designs:
+
+### There are 5 points that are worth to be discussed:
+#### 1- Nodes/Peers initialization process:
+The book [_Computer Networking: A Top-Down Approach by Kurose, sixth edition_](https://www.amazon.com/Computer-Networking-Top-Down-Approach-6th/dp/0132856204) does not mention any algorithm or means for the nodes/peers to connect and be aware of each other without using broadcast address, In this application, the initialization and configuration of the peers has to be done in the `INIT_CONFIG` environment variable to know current/previous/subsequent1/subsequent2 peers/nodes.
+
+#### 2- Consensus of peers/states:
+The algorithm does not guarantee a stable and highly available state of the peers as it does not have any mechanism to do so, it distributes the work based on the key to available nodes in the peers chain. One advantage that the current application have over the book's algorithm is that every node/peer is capable of serving the set and get commands.
+
+#### 3- Data:
+The data in a specific peer gets lost if the peer decides to leave, the data is distributed and not replicated.
+
+#### 4- Re-arrangement of keys:
+If a node decides to join/leave, the data the exists inside the already existing nodes/peers will stay as is and a re-arrangement is needed for optimal keys distribution, this is not accounted for in this application or the algorithm in the book.
+
+#### 5- Collision of hashes
+The algorithm in the book does not mention how to handle collision on hash handling and assumes that the keys are collision free despite that the algorithm used for hashing is extremely prone to collisions. The way I handled the collisions in this application is that for each hash key, I created an internal hash table that takes the original string key and an array of values, therefore, the searching algorithm will look if the hash exists, if so, it looks for the internal key, if it exists, it returns the array of values it has saved.
+
+
+## Sample output (comments on execution starts with #):
+```
+# Initialization
+dht16_1  | Node dht16 on index 16 listening on port 15000!
+dht12_1  | Node dht12 on index 12 listening on port 15000!
+dht4_1   | Node dht4 on index 4 listening on port 15000!
+dht1_1   | Node dht1 on index 1 listening on port 15000!
+dht8_1   | Node dht8 on index 8 listening on port 15000!
+dht16_1  | Started the heartbeating process.
+dht12_1  | Started the heartbeating process.
+dht4_1   | Started the heartbeating process.
+dht1_1   | Started the heartbeating process.
+dht8_1   | Started the heartbeating process.
+
+# Inset a key-value pair
+dht12_1  | Sending Some Great Book 1:Some Great Value 1 of hash 4 into node dht16@192.168.80.2 on index 16
+dht16_1  | Sending Some Great Book 1:Some Great Value 1 of hash 4 into node dht4@192.168.80.4 on index 4
+dht4_1   | Inserting Some Great Book 1:Some Great Value 1 of hash 4 into node dht4@192.168.80.4 on index 4
+dht12_1  | Sending Some Great Book 2:Some Great Value 2 of hash 5 into node dht16@192.168.80.2 on index 16
+dht16_1  | Sending Some Great Book 2:Some Great Value 2 of hash 5 into node dht1@192.168.80.5 on index 1
+dht1_1   | Sending Some Great Book 2:Some Great Value 2 of hash 5 into node dht4@192.168.80.4 on index 4
+dht4_1   | Sending Some Great Book 2:Some Great Value 2 of hash 5 into node dht8@192.168.80.6 on index 8
+dht8_1   | Inserting Some Great Book 2:Some Great Value 2 of hash 5 into node dht8@192.168.80.6 on index 8
+dht12_1  | Sending Some Great Book 2:Some Great Value 3 of hash 5 into node dht16@192.168.80.2 on index 16
+dht16_1  | Sending Some Great Book 2:Some Great Value 3 of hash 5 into node dht1@192.168.80.5 on index 1
+dht1_1   | Sending Some Great Book 2:Some Great Value 3 of hash 5 into node dht4@192.168.80.4 on index 4
+dht4_1   | Sending Some Great Book 2:Some Great Value 3 of hash 5 into node dht8@192.168.80.6 on index 8
+dht8_1   | Inserting Some Great Book 2:Some Great Value 3 of hash 5 into node dht8@192.168.80.6 on index 8
+dht12_1  | Sending Some Great Book 2:Some Great Value 4 of hash 5 into node dht16@192.168.80.2 on index 16
+dht16_1  | Sending Some Great Book 2:Some Great Value 4 of hash 5 into node dht1@192.168.80.5 on index 1
+dht1_1   | Sending Some Great Book 2:Some Great Value 4 of hash 5 into node dht4@192.168.80.4 on index 4
+dht4_1   | Sending Some Great Book 2:Some Great Value 4 of hash 5 into node dht8@192.168.80.6 on index 8
+dht8_1   | Inserting Some Great Book 2:Some Great Value 4 of hash 5 into node dht8@192.168.80.6 on index 8
+
+
+# Get a key
+dht1_1   | Searching for Some Great Book in node dht1@192.168.80.5 that has index 1...
+dht1_1   | Key Some Great Book NOT found in node dht1@192.168.80.5 ... Sending to subsequent node dht4@192.168.80.4...
+dht4_1   | Searching for Some Great Book in node dht4@192.168.80.4 that has index 4...
+dht4_1   | Key Some Great Book NOT found in node dht4@192.168.80.4 ... Sending to subsequent node dht8@192.168.80.6...
+dht8_1   | Searching for Some Great Book in node dht8@192.168.80.6 that has index 8...
+dht8_1   | Key Some Great Book NOT found in node dht8@192.168.80.6 ... Sending to subsequent node dht12@192.168.80.3...
+dht12_1  | Searching for Some Great Book in node dht12@192.168.80.3 that has index 12...
+dht12_1  | Key Some Great Book NOT found in node dht12@192.168.80.3 ... Sending to subsequent node dht16@192.168.80.2...
+dht16_1  | Searching for Some Great Book in node dht16@192.168.80.2 that has index 16...
+dht16_1  | Key Some Great Book NOT found in node dht16@192.168.80.2 ... Sending to subsequent node dht1@192.168.80.5...
+dht1_1   | The searching process for Some Great Book reached a loop, sending NOT FOUND to client
+dht1_1   | Searching for Some Great Book 1 in node dht1@192.168.80.5 that has index 1...
+dht1_1   | Key Some Great Book 1 NOT found in node dht1@192.168.80.5 ... Sending to subsequent node dht4@192.168.80.4...
+dht4_1   | Searching for Some Great Book 1 in node dht4@192.168.80.4 that has index 4...
+dht4_1   | Key Some Great Book 1 found in node dht4@192.168.80.4 that has index 4...
+dht1_1   | Searching for Some Great Book 2 in node dht1@192.168.80.5 that has index 1...
+dht1_1   | Key Some Great Book 2 NOT found in node dht1@192.168.80.5 ... Sending to subsequent node dht4@192.168.80.4...
+dht4_1   | Searching for Some Great Book 2 in node dht4@192.168.80.4 that has index 4...
+dht4_1   | Key Some Great Book 2 NOT found in node dht4@192.168.80.4 ... Sending to subsequent node dht8@192.168.80.6...
+dht8_1   | Searching for Some Great Book 2 in node dht8@192.168.80.6 that has index 8...
+dht8_1   | Key Some Great Book 2 found in node dht8@192.168.80.6 that has index 8...
+
+# Leave command
+dht4_1   | a LEAVE command was sent to node dht4@192.168.80.4.
+dht1_1   | Heartbeat from dht1 to dht4 failed, changing nodes chain.
+dht1_1   | Subsequent 1 hostname is now: dht8@192.168.80.6
+dht1_1   | Subsequent 2 hostname is now: dht12
+dht16_1  | Changed SUBSEQUENT2_HOSTNAME to dht8@192.168.80.6 with index of 8.
+
+# Join command
+dht4_1   | a JOIN command was sent to node dht4@192.168.80.4.
+dht1_1   | Changed SUBSEQUENT2_HOSTNAME to dht8@192.168.80.6 with index of 8.
+dht1_1   | Changed SUBSEQUENT1_HOSTNAME to dht4@192.168.80.4 with index of 4.
+dht8_1   | Changed PREVIOUS_HOSTNAME to dht4@192.168.80.4 with index of 4.
+dht16_1  | Changed SUBSEQUENT2_HOSTNAME to dht4@192.168.80.4 with index of 4.
+```
---
+++ app.js
@@ -0,0 +1,243 @@
+// talk about the initialization and node awareness issue
+// talk about the level of consensus between the nodes
+// talk about the data loss when a node leaves.
+// talk about the re-arrangement needed when a node enters/leaves
+// talk about collision
+
+var express = require('express');
+var request = require("request");
+var fs = require("fs");
+var app = express();
+var logger = console;
+const dns = require('dns');
+const util = require('util');
+const async = require('async');
+
+
+const SOCKET_PORT = process.env.SOCKET_PORT || 15000;
+if(!process.env.MODULO_VAL){
+    logger.log("ERROR: You need to set MODULO_VAL environment variable");
+    process.exit(1);
+}
+if(!process.env.INIT_CONFIG){
+    logger.log("ERROR: You need to set INIT_CONFIG environment variable");
+    process.exit(1);
+}
+const MODULO_VAL = parseInt(process.env.MODULO_VAL) || 5;
+let INIT_CONFIG = process.env.INIT_CONFIG;
+INIT_CONFIG = INIT_CONFIG.split("-");
+const CURRENT_HOSTNAME = INIT_CONFIG[0];
+const CURRENT_INDEX = parseInt(INIT_CONFIG[1]);
+let PREVIOUS_HOSTNAME = INIT_CONFIG[2];
+let PREVIOUS_INDEX = parseInt(INIT_CONFIG[3]);
+let SUBSEQUENT1_HOSTNAME = INIT_CONFIG[4];
+let SUBSEQUENT1_INDEX = parseInt(INIT_CONFIG[5]);
+let SUBSEQUENT2_HOSTNAME = INIT_CONFIG[6];
+let SUBSEQUENT2_INDEX = parseInt(INIT_CONFIG[7]);
+let livenessProbe = process.env.INIT_LIVENESS_PROBE || "OK";
+let ips = {};
+
+async function assignIPs(){
+    ips[CURRENT_HOSTNAME] = await util.promisify(dns.lookup)(CURRENT_HOSTNAME);
+    ips[PREVIOUS_HOSTNAME] = await util.promisify(dns.lookup)(PREVIOUS_HOSTNAME);
+    ips[SUBSEQUENT1_HOSTNAME] = await util.promisify(dns.lookup)(SUBSEQUENT1_HOSTNAME);
+    ips[SUBSEQUENT2_HOSTNAME] = await util.promisify(dns.lookup)(SUBSEQUENT2_HOSTNAME);
+    ips[CURRENT_HOSTNAME] = ips[CURRENT_HOSTNAME].address;
+    ips[PREVIOUS_HOSTNAME] = ips[PREVIOUS_HOSTNAME].address;
+    ips[SUBSEQUENT1_HOSTNAME] = ips[SUBSEQUENT1_HOSTNAME].address;
+    ips[SUBSEQUENT2_HOSTNAME] = ips[SUBSEQUENT2_HOSTNAME].address;
+}
+
+assignIPs();
+
+let hashTable = {};
+
+app.get("/insert/:key/:value", (req, res)=>{
+    let key = req.params.key;
+    let value = req.params.value;
+    let hash = calculateKeyHash(key);
+
+    // initial cases: if hash == any of the indexes that this node knows.
+    if(hash == CURRENT_INDEX) return insertIntoCurrent(key, value, hash, res);
+    if(hash == PREVIOUS_INDEX) return sendToNode(key, value, hash, PREVIOUS_INDEX, PREVIOUS_HOSTNAME, res);
+    if(hash == SUBSEQUENT1_INDEX) return sendToNode(key, value, hash, SUBSEQUENT1_INDEX, SUBSEQUENT1_HOSTNAME, res);
+    if(hash == SUBSEQUENT2_INDEX) return sendToNode(key, value, hash, SUBSEQUENT2_INDEX, SUBSEQUENT2_HOSTNAME, res);
+
+    // edge case 1: first node (current index < previous index)
+    if(CURRENT_INDEX < PREVIOUS_INDEX && hash > PREVIOUS_INDEX) return insertIntoCurrent(key, value, hash, res);
+
+    // edge case 2: last node (subsequent index < current index)
+    if(SUBSEQUENT1_INDEX < CURRENT_INDEX && hash > CURRENT_INDEX) return sendToNode(key, value, hash, SUBSEQUENT1_INDEX, SUBSEQUENT1_HOSTNAME, res);
+
+    // if the current hash is between previous and current (insert in current), otherwise send to subsequent
+    if(hash > PREVIOUS_INDEX && hash <= CURRENT_INDEX) return insertIntoCurrent(key, value, hash, res);
+    else return sendToNode(key, value, hash, SUBSEQUENT1_INDEX, SUBSEQUENT1_HOSTNAME, res);
+});
+
+app.get("/get/:key", (req, res)=>{
+    let key = req.params.key;
+    findingProcess(key, CURRENT_HOSTNAME, req, res);
+});
+
+app.get("/internal/get/:key/:hostname", (req, res)=>{
+    let key = req.params.key;
+    let hostname = req.params.hostname;
+    if(CURRENT_HOSTNAME == hostname){
+        logger.log(`The searching process for ${key} reached a loop, sending NOT FOUND to client`);
+        return res.send("NOT FOUND");
+    }
+    else {
+        findingProcess(key, hostname, req, res);
+    }
+});
+
+
+// heartbeating
+setTimeout(()=>{
+    logger.log("Started the heartbeating process.");
+    async.forever((next)=>{
+        request.get(`http://${SUBSEQUENT1_HOSTNAME}:${SOCKET_PORT}/heartbeat`, async (err, res, body)=>{
+            //logger.log(`${SUBSEQUENT1_HOSTNAME} body: `, body);
+            if(body !== "OK"){
+                logger.log(`Heartbeat from ${CURRENT_HOSTNAME} to ${SUBSEQUENT1_HOSTNAME} failed, changing nodes chain.`);
+                // change subsequent 1
+                SUBSEQUENT1_HOSTNAME = SUBSEQUENT2_HOSTNAME;
+                SUBSEQUENT1_INDEX = SUBSEQUENT2_INDEX;
+                ips[SUBSEQUENT1_HOSTNAME] = await util.promisify(dns.lookup)(SUBSEQUENT1_HOSTNAME);
+                ips[SUBSEQUENT1_HOSTNAME] = ips[SUBSEQUENT1_HOSTNAME].address;
+                logger.log(`Subsequent 1 hostname is now: ${SUBSEQUENT1_HOSTNAME}@${ips[SUBSEQUENT1_HOSTNAME]}`);
+
+                // change subsequent 2
+                request.get(`http://${SUBSEQUENT2_HOSTNAME}:${SOCKET_PORT}/internal/get/SUBSEQUENT1_HOSTNAME+":"+SUBSEQUENT1_INDEX`, async (err, res, body)=>{
+                    SUBSEQUENT2_HOSTNAME = body.split(":")[0];
+                    SUBSEQUENT2_INDEX = body.split(":")[1];
+                    ips[SUBSEQUENT2_HOSTNAME] = await util.promisify(dns.lookup)(SUBSEQUENT2_HOSTNAME);
+                    ips[SUBSEQUENT2_HOSTNAME] = ips[SUBSEQUENT2_HOSTNAME].address;
+                    logger.log(`Subsequent 2 hostname is now: ${SUBSEQUENT2_HOSTNAME}`);
+                });
+
+                // change previou's subsequent 2 to new subsequent 1
+                request.get(`http://${PREVIOUS_HOSTNAME}:${SOCKET_PORT}/internal/changehost/SUBSEQUENT2_HOSTNAME/${SUBSEQUENT1_HOSTNAME}/${SUBSEQUENT1_INDEX}`);
+            }
+        });
+        setTimeout(()=>{next();}, 5000);
+    });
+}, 10000);
+
+
+app.get("/heartbeat", (req, res)=>{res.send(livenessProbe)});
+// Note: this is not a secure route as it uses eval of what's sent to it.
+app.get("/internal/get/:varName", (req, res)=>{res.send(eval(req.params.varName))});
+
+app.get("/leave", (req, res)=>{
+    logger.log(`a LEAVE command was sent to node ${CURRENT_HOSTNAME}@${ips[CURRENT_HOSTNAME]}.`);
+    livenessProbe = "NOTOK";
+    res.send("Successfully left the node chain.");
+});
+
+app.get("/join", (req, res)=>{
+    logger.log(`a JOIN command was sent to node ${CURRENT_HOSTNAME}@${ips[CURRENT_HOSTNAME]}.`);
+    livenessProbe = "OK";
+    // 1- change subsequent 2 of previous x2
+    request.get(`http://${PREVIOUS_HOSTNAME}:${SOCKET_PORT}/internal/get/PREVIOUS_HOSTNAME+":"+PREVIOUS_INDEX`, async (err, res, body)=>{
+        let PREVIOUS2_HOSTNAME = body.split(":")[0];
+        let PREVIOUS2_INDEX = body.split(":")[1];
+        request.get(`http://${PREVIOUS2_HOSTNAME}:${SOCKET_PORT}/internal/changehost/SUBSEQUENT2_HOSTNAME/${CURRENT_HOSTNAME}/${CURRENT_INDEX}`);
+    });
+    // 2- change previous subsequent 1 to current
+    request.get(`http://${PREVIOUS_HOSTNAME}:${SOCKET_PORT}/internal/changehost/SUBSEQUENT1_HOSTNAME/${CURRENT_HOSTNAME}/${CURRENT_INDEX}`);
+    // 3- change previous subsequent 2 to subsequent 1
+    request.get(`http://${PREVIOUS_HOSTNAME}:${SOCKET_PORT}/internal/changehost/SUBSEQUENT2_HOSTNAME/${SUBSEQUENT1_HOSTNAME}/${SUBSEQUENT1_INDEX}`);
+    // 4- change subsequent 1 previous to current
+    request.get(`http://${SUBSEQUENT1_HOSTNAME}:${SOCKET_PORT}/internal/changehost/PREVIOUS_HOSTNAME/${CURRENT_HOSTNAME}/${CURRENT_INDEX}`);
+    res.send("Successfully joined the node chain.");
+});
+
+app.get("/internal/changehost/:hostType/:hostname/:index", async (req, res)=>{
+    let hostType = req.params.hostType;
+    let hostname = req.params.hostname;
+    let index = req.params.index;
+    // this can be eliminated if I used a dictionary to save/assign them.
+    switch (hostType) {
+        case "SUBSEQUENT1_HOSTNAME":
+            SUBSEQUENT1_HOSTNAME = hostname;
+            SUBSEQUENT1_INDEX = index;
+            ips[SUBSEQUENT1_HOSTNAME] = await util.promisify(dns.lookup)(SUBSEQUENT1_HOSTNAME);
+            ips[SUBSEQUENT1_HOSTNAME] = ips[SUBSEQUENT1_HOSTNAME].address;
+            logger.log(`Changed ${hostType} to ${hostname}@${ips[SUBSEQUENT1_HOSTNAME]} with index of ${index}.`);
+            break;
+        case "SUBSEQUENT2_HOSTNAME":
+            SUBSEQUENT2_HOSTNAME = hostname;
+            SUBSEQUENT2_INDEX = index;
+            ips[SUBSEQUENT2_HOSTNAME] = await util.promisify(dns.lookup)(SUBSEQUENT2_HOSTNAME);
+            ips[SUBSEQUENT2_HOSTNAME] = ips[SUBSEQUENT2_HOSTNAME].address;
+            logger.log(`Changed ${hostType} to ${hostname}@${ips[SUBSEQUENT2_HOSTNAME]} with index of ${index}.`);
+            break;
+        case "PREVIOUS_HOSTNAME":
+            PREVIOUS_HOSTNAME = hostname;
+            PREVIOUS_INDEX = index;
+            ips[PREVIOUS_HOSTNAME] = await util.promisify(dns.lookup)(PREVIOUS_HOSTNAME);
+            ips[PREVIOUS_HOSTNAME] = ips[PREVIOUS_HOSTNAME].address;
+            logger.log(`Changed ${hostType} to ${hostname}@${ips[PREVIOUS_HOSTNAME]} with index of ${index}.`);
+            break;
+        default:
+            break;
+    }
+    res.sendStatus(200);
+});
+
+function findingProcess(key, hostname, req, res){
+    logger.log(`Searching for ${key} in node ${CURRENT_HOSTNAME}@${ips[CURRENT_HOSTNAME]} that has index ${CURRENT_INDEX}...`)
+    let found = searchCurrentNode(key);
+    if(found){
+        logger.log(`Key ${key} found in node ${CURRENT_HOSTNAME}@${ips[CURRENT_HOSTNAME]} that has index ${CURRENT_INDEX}...`)
+        return res.send(found);
+    }
+    else {
+        logger.log(`Key ${key} NOT found in node ${CURRENT_HOSTNAME}@${ips[CURRENT_HOSTNAME]} ... Sending to subsequent node ${SUBSEQUENT1_HOSTNAME}@${ips[SUBSEQUENT1_HOSTNAME]}...`);
+        let internalPath = `http://${SUBSEQUENT1_HOSTNAME}:${SOCKET_PORT}/internal/get/${key}/${hostname}`;
+        req.pipe(request(internalPath)).pipe(res);
+    }
+}
+
+function searchCurrentNode(key){
+    let found = false;
+    let hash = calculateKeyHash(key);
+    if(hashTable[hash] && hashTable[hash][key]){
+        found = hashTable[hash][key];
+    }
+    return found;
+}
+
+function insertIntoCurrent(key, value, hash, res){
+    let message = `Inserting ${key}:${value} of hash ${hash} into node ${CURRENT_HOSTNAME}@${ips[CURRENT_HOSTNAME]} on index ${CURRENT_INDEX}`;
+    logger.log(message);
+    if(hashTable[hash]){
+        if(hashTable[hash][key]) hashTable[hash][key].push(value);
+        else hashTable[hash][key] = [value];
+    }
+    else {
+        hashTable[hash] = {};
+        hashTable[hash][key] = [value];
+    }
+    return res.send(message);
+}
+
+function sendToNode(key, value, hash, nodeIndex, nodeHostname, res){
+    let message = `Sending ${key}:${value} of hash ${hash} into node ${nodeHostname}@${ips[nodeHostname]} on index ${nodeIndex}`;
+    logger.log(message);
+    request.get(`http://${nodeHostname}:${SOCKET_PORT}/insert/${key}/${value}`);
+    return res.send(message);
+}
+
+function calculateKeyHash(key){
+    let sum = 0;
+    for (var i = 0; i < key.length; i++) {
+        sum += key.charCodeAt(i);
+    }
+    return (sum % MODULO_VAL) + 1;
+}
+
+app.listen(SOCKET_PORT, function () {
+    logger.log(`Node ${CURRENT_HOSTNAME} on index ${CURRENT_INDEX} listening on port ${SOCKET_PORT}!`)
+});
